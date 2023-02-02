c978eaa1c227bb9b4b5c831febd2e5b6b21e4628
unknown <calais@student.chalmers.se>
2023-02-02 07:37:19 +0100

    fibbonacital, git o pythontest

---
+++ adamstester.py
@@ -0,0 +1,11 @@
+fst_fibonacci = 1
+snd_fibonacci = 1
+fibonacci = [fst_fibonacci, snd_fibonacci]
+for i in range(2,10):
+    temp = fst_fibonacci
+    fst_fibonacci = snd_fibonacci
+    snd_fibonacci = fst_fibonacci + temp  
+    fibonacci.append(snd_fibonacci)
+
+
+print(fibonacci)
