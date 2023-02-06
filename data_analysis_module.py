06afac70084dc1e5a439316139db3fdc9e12988e
efraimzeta <85368404+efraimzeta@users.noreply.github.com>
2023-02-06 16:00:19 +0100

    Lagt till kalibrering av korset
    Ändrat image_processing till att kunna ta fram kryssets position

--- data_analysis_module.py
+++ data_analysis_module.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 class DataAnalysis:
+    # Input: bollens poition för kamera 1 och 2
     def __init__(self, positions1, positions2):
         self.positions1 = positions1
         self.positions2 = positions2
@@ -12,7 +13,7 @@ class DataAnalysis:
         velocity2, _ = curve_fit(lambda t, a, b: a*t + b,  time, self.positions2)
         return velocity1[0], velocity2[0]
         
-    def calculate_accuracy(self, target_position):
-        error1 = np.abs(self.positions1[-1] - target_position)
-        error2 = np.abs(self.positions2[-1] - target_position)
+    def calculate_accuracy(self, cross_position):
+        error1 = np.abs(self.positions1[-1] - cross_position)
+        error2 = np.abs(self.positions2[-1] - cross_position)
         return error1, error2
--- image_processing_module.py
+++ image_processing_module.py
@@ -1,14 +1,13 @@
 import cv2
 
 class ImageProcessing:
-    #video1 = cv2.VideoCapture("path/to/video1.mov")
-    #Kan ändra variabeln till en path till filen istället
-    #Kanske göra en till funktion här som enbart detekterar kryssets position
+    
     def __init__(self, video1, video2):
         self.video1 = video1
         self.video2 = video2
 
     def detect_dodgeball(self):
+        # Använder en cascade classifier som är specificerad för att urskilja dodgeballs i bilder.
         dodgeball_cascade = cv2.CascadeClassifier('dodgeball.xml')
         positions = []
         while True:
@@ -19,9 +18,11 @@ class ImageProcessing:
             dodgeballs1 = dodgeball_cascade.detectMultiScale(gray1, 1.3, 5)
             dodgeballs2 = dodgeball_cascade.detectMultiScale(gray2, 1.3, 5)
             for (x,y,w,h) in dodgeballs1:
+                #Gör en rektangel runt bollen och för in x-och y-koordinater för video1
                 cv2.rectangle(frame1,(x,y),(x+w,y+h),(255,0,0),2)
                 positions.append((x, y))
             for (x,y,w,h) in dodgeballs2:
+                #Gör en rektangel runt bollen och för in x-och y-koordinater för video2
                 cv2.rectangle(frame2,(x,y),(x+w,y+h),(255,0,0),2)
                 positions.append((x, y))
             cv2.imshow('Video 1',frame1)
@@ -31,4 +32,39 @@ class ImageProcessing:
         self.video1.release()
         self.video2.release()
         cv2.destroyAllWindows()
+        # returns poitionen för bollen i förhållande till det vöre vänstra hörnet av kamerans synfält.
         return positions
+
+    def calibrate_cross(self):
+        # Här håller vi en dodgeball mot krysset för att veta vad centrum är för krysset. 
+        # Ville egentligen detektera krysset direkt, 
+        # men verkar vara svårare än att detektera bollen av någon anledning, 
+        # så därför använder jag mig av bollen för att kalibrera krysset
+        dodgeball_cascade = cv2.CascadeClassifier('dodgeball.xml')
+        cross_positions = []
+        while True:
+            ret1, frame1 = self.video1.read()
+            ret2, frame2 = self.video2.read()
+            gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+            gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+            dodgeballs1 = dodgeball_cascade.detectMultiScale(gray1, 1.3, 5)
+            dodgeballs2 = dodgeball_cascade.detectMultiScale(gray2, 1.3, 5)
+            for (x,y,w,h) in dodgeballs1:
+                #Gör en rektangel runt bollen och för in x-och y-koordinater för video1
+                cv2.rectangle(frame1,(x,y),(x+w,y+h),(255,0,0),2)
+                cross_positions.append((x, y))
+            for (x,y,w,h) in dodgeballs2:
+                #Gör en rektangel runt bollen och för in x-och y-koordinater för video2
+                cv2.rectangle(frame2,(x,y),(x+w,y+h),(255,0,0),2)
+                cross_positions.append((x, y))
+            cv2.imshow('Video 1',frame1)
+            cv2.imshow('Video 2',frame2)
+            # Image processing av videon görs fram till dess att vi trycker på 'q'. 
+            # Behöver definiera om när vi ska sluta mäta bollens position.
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        self.video1.release()
+        self.video2.release()
+        cv2.destroyAllWindows()
+        # returns poitionen för bollen i förhållande till det vöre vänstra hörnet av kamerans synfält.
+        return cross_positions
