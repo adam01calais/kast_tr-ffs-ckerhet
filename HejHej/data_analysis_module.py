13ae12d891cf676ef6afa4229dd6eb7c116b254b
efraimzeta <85368404+efraimzeta@users.noreply.github.com>
2023-02-10 09:37:03 +0100

    Replace:ade data_analysis mot rätt

--- HejHej/data_analysis_module.py
+++ HejHej/data_analysis_module.py
@@ -3,17 +3,19 @@ from scipy.optimize import curve_fit
 
 class DataAnalysis:
     # Input: bollens poition för kamera 1 och 2
-    def __init__(self, positions1, positions2):
-        self.positions1 = positions1
-        self.positions2 = positions2
+    def __init__(self, dodgeball_position_x, dodgeball_position_y):
+        self.dodgeball_position_x = dodgeball_position_x
+        self.dodgeball_position_y = dodgeball_position_y
 
     def calculate_velocity(self):
-        time = np.linspace(0, len(self.positions1), len(self.positions1))
-        velocity1, _ = curve_fit(lambda t, a, b: a*t + b,  time, self.positions1)
-        velocity2, _ = curve_fit(lambda t, a, b: a*t + b,  time, self.positions2)
+        time = np.linspace(0, len(self.dodgeball_position_x), len(self.dodgeball_position_y))
+        velocity1, _ = curve_fit(lambda t, a, b: a*t + b,  time, self.dodgeball_position_x)
+        velocity2, _ = curve_fit(lambda t, a, b: a*t + b,  time, self.dodgeball_position_y)
         return velocity1[0], velocity2[0]
         
-    def calculate_accuracy(self, cross_position):
-        error1 = np.abs(self.positions1[-1] - cross_position)
-        error2 = np.abs(self.positions2[-1] - cross_position)
-        return error1, error2
+    def calculate_accuracy(self, cross_position_x, cross_position_y):
+        # error_x och error_y är avståndet från träffpunkten i antalet pixlar
+        # Behöver konverteras till cm (kanske?) eller annan längdenhet
+        error_x = self.dodgeball_position_x[-1] - cross_position_x
+        error_y = self.dodgeball_position_y[-1] - cross_position_y
+        return error_x, error_y
