1e629f911aff4eb4b97791ef66b45c0aa6a9693f
efraimzeta <85368404+efraimzeta@users.noreply.github.com>
2023-02-10 10:46:14 +0100

    Update efraimstester.py

--- Else/efraimstester.py
+++ Else/efraimstester.py
@@ -1,29 +1,48 @@
+# import cv2
+
+# # Load the video file
+# video = cv2.VideoCapture("/Users/efraimzetterqvist/Library/Mobile Documents/com~apple~CloudDocs/Documents/IMG_2768.mov")
+
+# # Check if the video was successfully opened
+# if not video.isOpened():
+#     print("Error: Could not open the video file.")
+#     exit()
+
+# # Loop over each frame of the video
+# while True:
+#     # Read the next frame from the video
+#     ret, frame = video.read()
+
+#     # Break out of the loop if we have reached the end of the video
+#     if not ret:
+#         break
+
+#     # Display the current frame
+#     cv2.imshow("Video", frame)
+
+#     # Break out of the loop if the user presses the 'q' key
+#     if cv2.waitKey(25) & 0xFF == ord('q'):
+#         break
+
+# # Release the video file and close all windows
+# video.release()
+# cv2.destroyAllWindows()
+
 import cv2
 
-# Load the video file
-video = cv2.VideoCapture("/Users/efraimzetterqvist/Library/Mobile Documents/com~apple~CloudDocs/Documents/IMG_2768.mov")
+# Load the image file
+img = cv2.imread("image.jpg")
 
-# Check if the video was successfully opened
-if not video.isOpened():
-    print("Error: Could not open the video file.")
+# Check if the image was successfully loaded
+if img is None:
+    print("Error: Could not load the image file.")
     exit()
 
-# Loop over each frame of the video
-while True:
-    # Read the next frame from the video
-    ret, frame = video.read()
+# Display the image
+cv2.imshow("Image", img)
 
-    # Break out of the loop if we have reached the end of the video
-    if not ret:
-        break
+# Wait for the user to close the window
+cv2.waitKey(0)
 
-    # Display the current frame
-    cv2.imshow("Video", frame)
-
-    # Break out of the loop if the user presses the 'q' key
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
-
-# Release the video file and close all windows
-video.release()
+# Close all windows
 cv2.destroyAllWindows()
