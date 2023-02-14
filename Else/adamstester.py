eacb8dd5fc8a8408a897f8446f04344bd7d3ab0d
Adam Calais <calais@student.chalmers.se>
2023-02-14 11:39:06 +0100

    Even more done

--- Else/adamstester.py
+++ Else/adamstester.py
@@ -9,9 +9,20 @@ pixles_on_camera_Rare_Wide = int(subPrinted[subPrinted.find("MP")-3:subPrinted.f
 camera_aparature_Rare_Wide = subPrinted[(subPrinted.find("MP")+4) : (subPrinted.find("MP"))+10]
 short_lens_Rare_Wide = int(subPrinted[subPrinted.find("mm")-3: subPrinted.find("mm")])/1000
 image_sensor_size_Rare_Wide = subPrinted[subPrinted.find("mm")+4:subPrinted.find("mm")+7]
+pixle_size_Rare_Wide = int(subPrinted[subPrinted.find("µm")-2: subPrinted.find("µm")])*0.0000001
 
-print(pixles_on_camera_Rare_Wide)
-print(camera_aparature_Rare_Wide)
-print(short_lens_Rare_Wide) 
-print(image_sensor_size_Rare_Wide)
+rare_camera_wide_stats = [pixles_on_camera_Rare_Wide,camera_aparature_Rare_Wide, short_lens_Rare_Wide,image_sensor_size_Rare_Wide,pixle_size_Rare_Wide]
+print(rare_camera_wide_stats)
+
+subPrinted = subPrinted[subPrinted.find("(wide)"):]
+
+pixles_on_camera_Rare_UltraWide = int(subPrinted[subPrinted.find("MP")-3:subPrinted.find("MP")])*1000000
+camera_aparature_Rare_UltraWide = subPrinted[(subPrinted.find("MP")+4) : (subPrinted.find("MP"))+10]
+short_lens_Rare_UltraWide = int(subPrinted[subPrinted.find("mm")-3: subPrinted.find("mm")])/1000
+image_sensor_size_Rare_UltraWide = subPrinted[subPrinted.find("mm")+4:subPrinted.find("mm")+7]
+pixle_size_Rare_UltraWide = int(subPrinted[subPrinted.find("µm")-2: subPrinted.find("µm")])*0.0000001
+
+rare_camera_ultrawide_stats = [pixles_on_camera_Rare_UltraWide,camera_aparature_Rare_UltraWide, short_lens_Rare_UltraWide,image_sensor_size_Rare_UltraWide,pixle_size_Rare_UltraWide]
+print(rare_camera_ultrawide_stats)
+
 # Kommer börja test-scrapea här.
