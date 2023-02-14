6edf560598dd1403c2d5e58fb7d8cf95eb43d092
unknown <calais@student.chalmers.se>
2023-02-14 13:17:00 +0100

    More stats

--- Else/adamstester.py
+++ Else/adamstester.py
@@ -9,7 +9,7 @@ pixles_on_camera_Rare_Wide = int(subPrinted[subPrinted.find("MP")-3:subPrinted.f
 camera_aparature_Rare_Wide = subPrinted[(subPrinted.find("MP")+4) : (subPrinted.find("MP"))+10]
 short_lens_Rare_Wide = int(subPrinted[subPrinted.find("mm")-3: subPrinted.find("mm")])/1000
 image_sensor_size_Rare_Wide = subPrinted[subPrinted.find("mm")+4:subPrinted.find("mm")+7]
-pixle_size_Rare_Wide = int(subPrinted[subPrinted.find("µm")-2: subPrinted.find("µm")])*0.0000001
+pixle_size_Rare_Wide = float(subPrinted[subPrinted.find("µm")-5: subPrinted.find("µm")])/1000000
 
 rare_camera_wide_stats = [pixles_on_camera_Rare_Wide,camera_aparature_Rare_Wide, short_lens_Rare_Wide,image_sensor_size_Rare_Wide,pixle_size_Rare_Wide]
 print(rare_camera_wide_stats)
@@ -19,14 +19,48 @@ subPrinted = subPrinted[subPrinted.find("(wide)"):]
 pixles_on_camera_Rare_UltraWide = int(subPrinted[subPrinted.find("MP")-3:subPrinted.find("MP")])*1000000
 camera_aparature_Rare_UltraWide = subPrinted[(subPrinted.find("MP")+4) : (subPrinted.find("MP"))+10]
 short_lens_Rare_UltraWide = int(subPrinted[subPrinted.find("mm")-3: subPrinted.find("mm")])/1000
-image_sensor_size_Rare_UltraWide = subPrinted[subPrinted.find("mm")+4:subPrinted.find("mm")+7]
-pixle_size_Rare_UltraWide = int(subPrinted[subPrinted.find("µm")-2: subPrinted.find("µm")])*0.0000001
 
-rare_camera_ultrawide_stats = [pixles_on_camera_Rare_UltraWide,camera_aparature_Rare_UltraWide, short_lens_Rare_UltraWide,image_sensor_size_Rare_UltraWide,pixle_size_Rare_UltraWide]
+
+rare_camera_ultrawide_stats = [pixles_on_camera_Rare_UltraWide,camera_aparature_Rare_UltraWide, short_lens_Rare_UltraWide]
 print(rare_camera_ultrawide_stats)
 
-<<<<<<< HEAD
+subPrinted = subPrinted[subPrinted.find("(ultrawide)"):]
+
+pixles_on_camera_Rare_Macro = int(subPrinted[subPrinted.find("MP")-2:subPrinted.find("MP")])*1000000
+camera_aparature_Rare_Macro = subPrinted[(subPrinted.find("MP")+4) : (subPrinted.find("MP"))+10]
+pixle_size_Rare_Macro = float(subPrinted[subPrinted.find("µm")-5: subPrinted.find("µm")-1])/1000000
+
+rare_camera_macro_stats = [pixles_on_camera_Rare_Macro,camera_aparature_Rare_Macro, pixle_size_Rare_Macro]
+print(rare_camera_macro_stats)
+
+subPrinted = subPrinted[subPrinted.find("(macro)"):]
+
+pixles_on_camera_Rare_Wide_Pro = int(subPrinted[subPrinted.find("MP")-3:subPrinted.find("MP")])*1000000
+camera_aparature_Rare_Wide_Pro = subPrinted[(subPrinted.find("MP")+4) : (subPrinted.find("MP"))+10]
+short_lens_Rare_Wide_Pro = int(subPrinted[subPrinted.find("mm")-3: subPrinted.find("mm")])/1000
+image_sensor_size_Rare_Wide_Pro = subPrinted[subPrinted.find("mm")+4:subPrinted.find("mm")+10]
+pixle_size_Rare_Wide_Pro = float(subPrinted[subPrinted.find("µm")-5: subPrinted.find("µm")])/1000000
+
+rare_camera_wide_stats_Pro = [pixles_on_camera_Rare_Wide_Pro,camera_aparature_Rare_Wide_Pro, short_lens_Rare_Wide_Pro,image_sensor_size_Rare_Wide_Pro,pixle_size_Rare_Wide_Pro]
+print(rare_camera_wide_stats_Pro)
+
+subPrinted = subPrinted[subPrinted.find("(ultrawide)"):]
+
+pixles_on_camera_Rare_Telephoto_Pro = int(subPrinted[subPrinted.find("MP")-2:subPrinted.find("MP")])*1000000
+camera_aparature_Rare_Telephoto_Pro = subPrinted[(subPrinted.find("MP")+4) : (subPrinted.find("MP"))+10]
+pixle_size_Rare_Telephoto_Pro = float(subPrinted[subPrinted.find("µm")-5: subPrinted.find("µm")-1])/1000000
+
+rare_camera_Telephoto_stats_Pro = [pixles_on_camera_Rare_Telephoto_Pro, camera_aparature_Rare_Telephoto_Pro, pixle_size_Rare_Telephoto_Pro]
+print(rare_camera_Telephoto_stats_Pro)
+
+subPrinted = subPrinted[subPrinted.find("(telephoto)"):]
+
+pixles_on_camera_Rare_Depth_Pro = int(subPrinted[subPrinted.find("MP")-2:subPrinted.find("MP")])*1000000
+camera_aparature_Rare_Depth_Pro = subPrinted[(subPrinted.find("MP")+4) : (subPrinted.find("MP"))+10]
+
+rare_camera_Depth_stats_Pro = [pixles_on_camera_Rare_Depth_Pro, camera_aparature_Rare_Depth_Pro]
+print(rare_camera_Depth_stats_Pro)
+
+subPrinted = subPrinted[subPrinted.find("Both"):]
+
 # Kommer börja test-scrapea här.
-=======
-# Kommer börja test-scrapea här.
->>>>>>> 3f71dc678f83f41a995baef7de5797bddf98ee65
