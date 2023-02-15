e685c04865884fd266ff0c816949914f2b10c9e6
efraimzeta <85368404+efraimzeta@users.noreply.github.com>
2023-02-15 13:52:22 +0100

    hsddhj

--- Else/efraimstester.py
+++ Else/efraimstester.py
@@ -9,6 +9,7 @@ w = []
 h = []
 
 video=cv2.VideoCapture("/Users/efraimzetterqvist/Library/Mobile Documents/com~apple~CloudDocs/Chalmers/IMG_1159.mov")
+
 i=0
 path= "/Users/efraimzetterqvist/Documents/dodge"
 while(video.isOpened()):
@@ -20,15 +21,15 @@ while(video.isOpened()):
 video.release()
 cv2.destroyAllWindows()
 
-# rf = Roboflow(api_key="CPkBglSIfMhKhrghnYcq")
-# project = rf.workspace().project("dodgeball-detection")
-# model = project.version(1).model
+rf = Roboflow(api_key="CPkBglSIfMhKhrghnYcq")
+project = rf.workspace().project("dodgeball-detection")
+model = project.version(1).model
 
-# for k in range(1000,1003):
-#     prediction = model.predict("/Users/efraimzetterqvist/Documents/dodge/dodge" + str(k) + '.jpg')
-#     for result in prediction.json()['predictions']:
-#         x.append(result['x'])
-#         y.append(result['y'])
-#         w.append(result['width'])
-#         h.append(result['height'])
-# print(x, y, w, h)
+for k in range(1000,1003):
+    prediction = model.predict("/Users/efraimzetterqvist/Documents/dodge/dodge" + str(k) + '.jpg')
+    for result in prediction.json()['predictions']:
+        x.append(result['x'])
+        y.append(result['y'])
+        w.append(result['width'])
+        h.append(result['height'])
+print(x, y, w, h)
