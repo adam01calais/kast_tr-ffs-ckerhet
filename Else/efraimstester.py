086492406cd502a67e04f60f575ef1a4b2d36927
efraimzeta <85368404+efraimzeta@users.noreply.github.com>
2023-02-15 13:12:02 +0100

    Update efraimstester.py

--- Else/efraimstester.py
+++ Else/efraimstester.py
@@ -1,33 +1,31 @@
 
-# import cv2
-# import os
+import cv2
+import os
+from roboflow import Roboflow
 
-# video=cv2.VideoCapture("/Users/efraimzetterqvist/Library/Mobile Documents/com~apple~CloudDocs/Chalmers/IMG_1159.mov")
-# i=0
-# path= "/Users/efraimzetterqvist/Documents/dodge"
-# while(video.isOpened()):
-#     ret, frame = video.read()
-#     if ret == False:
-#         break
-#     cv2.imwrite(os.path.join(path,'dodge'+str(i)+'.jpg'),frame)
-#     i+=1
+x = []
+y = []
+w = []
+h = []
 
-# video.release()
-# cv2.destroyAllWindows()
-
-# from roboflow import Roboflow
+video=cv2.VideoCapture("/Users/efraimzetterqvist/Library/Mobile Documents/com~apple~CloudDocs/Chalmers/IMG_1159.mov")
+i=0
+path= "/Users/efraimzetterqvist/Documents/dodge"
+while(video.isOpened()):
+    ret, frame = video.read()
+    if ret == False:
+        break
+    cv2.imwrite(os.path.join(path,'dodge'+str(i)+'.jpg'),frame)
+    i+=1
+video.release()
+cv2.destroyAllWindows()
 
 # rf = Roboflow(api_key="CPkBglSIfMhKhrghnYcq")
 # project = rf.workspace().project("dodgeball-detection")
 # model = project.version(1).model
 
-# x = []
-# y = []
-# w = []
-# h = []
-
-# for k in range(1000,1010):
-#     prediction = model.predict("/Users/efraimzetterqvist/Documents/dodge/dodge"+str(k)+'.jpg')
+# for k in range(1000,1003):
+#     prediction = model.predict("/Users/efraimzetterqvist/Documents/dodge/dodge" + str(k) + '.jpg')
 #     for result in prediction.json()['predictions']:
 #         x.append(result['x'])
 #         y.append(result['y'])
