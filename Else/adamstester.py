e3319523860aa68a0e2688c0f7ff3a0bef7137e5
unknown <calais@student.chalmers.se>
2023-02-16 07:22:11 +0100

    Trying to reduce the code even more

--- Else/adamstester.py
+++ Else/adamstester.py
@@ -6,33 +6,41 @@ printed = str(bsObj.prettify)
 subPrinted = printed[printed.find(
     "Rear camera</th>"):printed.find("Connectivity")]
 subPrinted = subPrinted + "<br/>"
+str_list = ["MP", "/", "mm" , "1/2", "µm"]
 
 
 
 for i in (0,2):
     subPrinted = subPrinted[subPrinted.find("</b>")+4:]
     subPrinted2 = subPrinted[0: subPrinted.find("</b>")]
+   
 
     while"MP" in subPrinted2:
+        j = 0
         pixles_on_camera_Rare = float(
-            subPrinted2[0:subPrinted2.find("MP")])*1000000
+            subPrinted2[0:subPrinted2.find(str_list[j])])*1000000
 
         subPrinted2 = subPrinted2[subPrinted.find(",")+1:]
+        j+=1
         camera_aparature_Rare = "f/" + \
-        subPrinted2[(subPrinted2.find("/")+1): (subPrinted2.find(","))]
+        subPrinted2[(subPrinted2.find(str_list[j])+1): (subPrinted2.find(","))]
         subPrinted2 = subPrinted2[subPrinted2.find(",")+1:]
-        if "mm" in subPrinted2[0:10]:
-            short_lens_Rare = float(subPrinted2[0: subPrinted2.find("mm")])/1000
+
+        j+=1
+        if str_list[j] in subPrinted2[0:10]:
+            short_lens_Rare = float(subPrinted2[0: subPrinted2.find(str_list[j])])/1000
             subPrinted2 = subPrinted2[subPrinted.find(",")+1:]
         else:
             short_lens_Rare = 0
-        if "1/2" in subPrinted2[0:10]:
+        j+=1
+        if str_list[j] in subPrinted2[0:10]:
             image_sensor_size_Rare = subPrinted2[0:subPrinted2.find(",")]
             subPrinted2 = subPrinted2[subPrinted.find(",")+1:]
         else:
             image_sensor_size_Rare = 0
-        if "µm" in subPrinted2[0:10]:
-            pixle_size_Rare = float(subPrinted2[0: subPrinted2.find("µm")])/1000000
+        j+=1
+        if str_list[j] in subPrinted2[0:10]:
+            pixle_size_Rare = float(subPrinted2[0: subPrinted2.find(str_list[j])])/1000000
             subPrinted2 = subPrinted2[subPrinted.find(",")+1:]
         else:
             pixle_size_Rare = 0
