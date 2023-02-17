c3a8ff4bdc3fad269a7e4e893d950eb80f26a205
efraimzeta <85368404+efraimzeta@users.noreply.github.com>
2023-02-17 23:02:43 +0100

    Update image_processing_module.py

--- HejHej/image_processing_module.py
+++ HejHej/image_processing_module.py
@@ -5,70 +5,122 @@ import shutil
 
 class ImageProcessing:
 
-    def __init__(self, video_floor, video_side):
-        self.video_floor = cv2.VideoCapture(video_floor)
-        self.video_side = cv2.VideoCapture(video_side)
-        self.cross_position_x_floor=[]
-        self.cross_position_y_floor=[]
-        self.cross_position_x_side=[]
-        self.cross_position_y_side=[]
-        self.dodgeball_position_x=[]
-        self.dodgeball_position_y=[]
+    def __init__(self, directory_path):
+        self.directory_path = directory_path
+        self.folder_name = "dodge"
+    
+    def calibrate_cross(self, video_path, camera_angle):
 
-    def detect(self, directory_path, video_name, list_of_images_numbers):
+        # Skapar den tomma mappen 'dodge' på plats vald av användaren
+        os.mkdir(os.path.join(self.directory_path, self.folder_name))
+        print(f"Created folder {self.folder_name} in directory {self.directory_path}")
 
-        #video_name= "IMG_1159 2.mov"
-        #directory_path = "/Users/efraimzetterqvist/Documents"
+        # Öppnar kalibreringsvideon
+        video = cv2.VideoCapture(video_path)
 
-        folder_name = "dodge"
-        os.mkdir(os.path.join(directory_path, folder_name))
-        print(f"Created folder {folder_name} in directory {directory_path}")
-
-        x = []
-        y = []
-        w = []
-        h = []
-
-        video = cv2.VideoCapture(directory_path + "/" + video_name)
-
+        # Delar upp videon i frames som placeras i mappen 'dodge'
         i=0
-        path= directory_path + "/" + folder_name
+        path = self.directory_path + "/" + self.folder_name
         while(video.isOpened()):
             ret, frame = video.read()
             if ret == False:
                 break
             cv2.imwrite(os.path.join(path,'dodge'+str(i)+'.jpg'),frame)
             i+=1
+        frame_rate = int(video.get(cv2.CAP_PROP_FPS))
+
+        # Stänger ner videon
         video.release()
         cv2.destroyAllWindows()
 
+        # Tillkallar en i Roboflow tränad modell för att detektera fotbollar
         rf = Roboflow(api_key="CPkBglSIfMhKhrghnYcq")
         project = rf.workspace().project("dodgeball-detection")
         model = project.version(1).model
 
+        # Gör en lista över de frames där detektering av dodgeball är aktuell för kalibrering
+        list_of_images_numbers = list(range(1, i, frame_rate))
+
+        # Skapar listor för bildens x- & y-koordinat
+        x = []
+        y = []
+
+        # Detekterar bollen för de aktuella framesen och stoppar kalibreringen då 
+        # då bollen varit relativt stilla mellan två aktuella frames (1 sekund)
         for k in list_of_images_numbers:
-            prediction = model.predict(path+"/dodge" + str(k) + '.jpg')
+            prediction = model.predict(self.directory_path + "/" + self.folder_name + "/dodge" + str(k) + '.jpg')
             for result in prediction.json()['predictions']:
                 x.append(result['x'])
                 y.append(result['y'])
-                w.append(result['width'])
-                h.append(result['height'])
-        print(x, y, w, h)
+            if len(x) < 2:
+                continue
+            if len(y) < 2:
+                continue
+            if max(abs(x[-2] - x[-1]), abs(y[-2] - y[-1])) < 3:
+                print('Calibration for ' + camera_angle + ' camera done successfully')
+                break
+        
+        # Sparar kryssets x- & y-koordinat i två variabler 
+        cross_position_x = x[-1]
+        cross_position_y = y[-1]
 
-        shutil.rmtree(path)       
-    
-    def calibrate_cross(self, directory_path, video_name):
-        cross_position_x=[]
-        cross_position_y=[]
-        video = cv2.VideoCapture(directory_path + "/" + video_name)
-        frame_rate = video.get(cv2.CAP_PROP_FPS)
-        frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        print("Frame rate: ", frame_rate, "Frame width: ", frame_width, "Frame_height: ", frame_height)
+        # Raderar mappen 'dodge' innehållandes alla frames
+        shutil.rmtree(self.directory_path + '/' + self.folder_name)  
+
+        # Skriver ut kryssets koordinater samt returnerar dem
+        print('x: ' + str(cross_position_x), 'y: ' + str(cross_position_y))  
+        return cross_position_x, cross_position_y   
+
+    def measure_throw(self, video_path, camera_angle):
+
+        # Skapar den tomma mappen 'dodge' på plats vald av användaren
+        os.mkdir(os.path.join(self.directory_path, self.folder_name))
+        print(f"Created folder {self.folder_name} in directory {self.directory_path}")
+
+        # Öppnar kastvideon
+        video = cv2.VideoCapture(video_path)
+
+        # Delar upp videon i frames som placeras i mappen 'dodge'
+        i=0
+        path = self.directory_path + "/" + self.folder_name
+        while(video.isOpened()):
+            ret, frame = video.read()
+            if ret == False:
+                break
+            cv2.imwrite(os.path.join(path,'dodge'+str(i)+'.jpg'),frame)
+            i+=1
+
+        # Stänger ner videon
         video.release()
-        i=range()
-        while True:
+        cv2.destroyAllWindows()
 
-            
-#image=ImageProcessing("/Users/efraimzetterqvist/Documents/IMG_1159.mov", "/Users/efraimzetterqvist/Documents/IMG_1159.mov") 
-#detected_ball=image.detect("/Users/efraimzetterqvist/Documents", "IMG_1159.mov", [1000,1001,1002])
+        # Tillkallar en i Roboflow tränad modell för att detektera fotbollar
+        rf = Roboflow(api_key="CPkBglSIfMhKhrghnYcq")
+        project = rf.workspace().project("dodgeball-detection")
+        model = project.version(1).model
+
+        # Skapar listor för bildens x- & y-koordinat
+        x = []
+        y = []
+
+        for k in range(1,i):
+            prediction = model.predict(self.directory_path + "/" + self.folder_name + "/dodge" + str(k) + '.jpg')
+            for result in prediction.json()['predictions']:
+                x.append(result['x'])
+                y.append(result['y'])
+            print(x, y)
+            k+=1
+            if len(x) < 2:
+                continue
+            if len(y) < 2:
+                continue
+            if x[-1] > x[-2]:
+                print('Throw for ' + camera_angle + ' successfully measured')
+                break
+
+        # Raderar mappen 'dodge' innehållandes alla frames
+        shutil.rmtree(self.directory_path + '/' + self.folder_name)  
+
+        # Skriver ut kryssets koordinater samt returnerar dem
+        print('Bollens positioner i x: ' + x, 'Bollens position i y: ' + y)  
+        return x, y 
