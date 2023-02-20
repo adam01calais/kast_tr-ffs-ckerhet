1dc5fb5932909c6b609b4ed176dab16b18c52f06
GabrielLjung <124040947+GabrielLjung@users.noreply.github.com>
2023-02-20 21:40:47 +0100

    Add files via upload

---
+++ Else/Webcam.py
@@ -0,0 +1,28 @@
+import cv2
+import time
+import datetime
+
+cap = cv2.VideoCapture(0)
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 30.0, frame_size)
+
+
+while True: 
+        _, frame = cap.read()
+
+        out.write(frame)
+
+        cv2.imshow("Camera", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+             break
+        
+        
+out.release()       
+cap.release()
+cv2.destroyAllWindows()
+
+        
