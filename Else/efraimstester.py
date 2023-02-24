beab4e5567ddf9c7bd458e4a36742b8c2de3048d
efraimzeta <85368404+efraimzeta@users.noreply.github.com>
2023-02-24 19:30:13 +0100

    jkj

--- Else/efraimstester.py
+++ Else/efraimstester.py
@@ -1,95 +1,61 @@
 import cv2
-import numpy as np
 
-# Open the video file for reading
-cap = cv2.VideoCapture('/Users/efraimzetterqvist/Documents/IMG_4557.mov')
+# Load the video and create a VideoCapture object
+cap = cv2.VideoCapture('/Users/efraimzetterqvist/Documents/IMG_1161.mov')
 
-# Define the parameters for Lucas-Kanade optical flow
-lk_params = dict(winSize=(15, 15),
-                 maxLevel=2,
-                 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
-                 flags=cv2.OPTFLOW_LK_GET_MIN_EIGENVALS)
+# Extract the first frame as the background image
+_, bg = cap.read()
+bg_gray = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
 
-# Take the first frame of the video
-ret, old_frame = cap.read()
+# Define the threshold for detecting the ball
+threshold = 100
 
-# Convert the first frame to grayscale
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+# Define the minimum area for the ball
+min_area = 500
 
-# Define the region of interest (ROI) where you want to track the optical flow
-# You can modify these values depending on your use case
-x, y, w, h = 300, 350,700, 500
-
-# Create a mask to visualize the ROI
-mask = np.zeros_like(old_frame)
-cv2.rectangle(mask, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-# Display the mask and first frame side by side
-cv2.imshow('ROI Mask', mask)
-cv2.imshow('First Frame', old_frame)
-
-# Define the initial points to track
-p0 = cv2.goodFeaturesToTrack(old_gray, maxCorners=50, qualityLevel=0.01, minDistance=10, blockSize=7)
-
-# Create a color map for visualizing the optical flow
-color_map = np.random.randint(0, 255, (50, 3))
-
-# Initialize the variables for tracking speed and direction
-speed = []
-direction = []
-
-# Start the loop to read each frame and track the optical flow
+# Loop through the video frames
 while True:
+    # Read the current frame
     ret, frame = cap.read()
-
     if not ret:
         break
 
     # Convert the current frame to grayscale
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Calculate the optical flow using Lucas-Kanade method
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    # Subtract the background from the current frame
+    diff = cv2.absdiff(bg_gray, frame_gray)
 
-    # Select only the points that were successfully tracked
-    good_new = p1[st == 1]
-    good_old = p0[st == 1]
+    # Apply a threshold to the difference image
+    _, thresh = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
 
-    # Draw the optical flow vectors
-    for i, (new, old) in enumerate(zip(good_new, good_old)):
-        a, b = new.ravel().astype(int)
-        c, d = old.ravel().astype(int)
-        mask = cv2.line(mask, (a, b), (c, d), color_map[i].tolist(), 1)
-        frame = cv2.circle(frame, (a, b), 3, color_map[i].tolist(), -1)
-        
-        a, b = new.ravel().astype(float)
-        c, d = old.ravel().astype(float)
+    # Apply morphological operations to remove noise
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    thresh = cv2.erode(thresh, kernel, iterations=2)
+    thresh = cv2.dilate(thresh, kernel, iterations=2)
 
-        # Compute the magnitude and angle of the optical flow vectors
-        magnitude, angle = cv2.cartToPolar(np.array([a]) - np.array([c]), np.array([b]) - np.array([d]))
-        
+    # Find contours in the binary image
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Convert the angle to degrees
-        angle = np.rad2deg(angle)
+    # Loop through the contours
+    for contour in contours:
+        # Calculate the area of the contour
+        area = cv2.contourArea(contour)
 
-        # Compute the speed and direction of the optical flow vectors
-        speed.append(magnitude)
-        direction.append(angle)
+        # If the area is larger than the minimum area, it is likely a ball
+        if area > min_area:
+            # Draw a bounding box around the ball
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    # Show the optical flow vectors on the current frame
-    img = cv2.add(frame, mask)
-    cv2.imshow('Optical Flow Vectors', img)
+    # Display the current frame
+    cv2.imshow('frame', frame)
 
-    # Update the variables for the next iteration
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1, 1, 2)
-
-    # Exit the loop if the user presses the '
-    if cv2.waitKey(1) == ord('q'):
+    # Check for key press
+    key = cv2.waitKey(1)
+    if key == ord('q'):
         break
 
-# Release video capture and close all windows
+# Release the VideoCapture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
-print(speed)
-#print(direction)
