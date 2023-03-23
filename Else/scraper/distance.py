f47afd4bf2ef8fd77755b09f2116aba357b5b4b9
unknown <calais@student.chalmers.se>
2023-03-23 08:37:22 +0100

    Final conclusion, for now

--- Else/scraper/distance.py
+++ Else/scraper/distance.py
@@ -4,9 +4,4 @@ from scipy.special import lambertw
 from math import tan
 
 OnePlus8 = Phone_Camera("https://en.wikipedia.org/wiki/OnePlus_8")
-i= 0.0357143*lambertw(16.0604*sqrt(OnePlus8.megaPixels))
-o = i*tan(1)/(0.000000560*OnePlus8.apperature*i-tan(1))
-
-print(OnePlus8.apperature)
-print(OnePlus8.megaPixels)
-print(o.real)
+print(OnePlus8.general_distance)
