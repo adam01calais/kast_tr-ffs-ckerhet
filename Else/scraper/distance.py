84f1a3f99b45ab4563902cc501c44cfead4cfbeb
Adam Calais <calais@student.chalmers.se>
2023-04-10 19:08:36 +0200

    uppdate

--- Else/scraper/distance.py
+++ Else/scraper/distance.py
@@ -4,9 +4,16 @@ from math import sqrt
 general_phone_thickness = 8.91/1000
 k = 10000000
 
-OnePlus8 = Phone_Camera("https://en.wikipedia.org/wiki/OnePlus_8")
-term1 = (-k*(OnePlus8.apperature) + 2*(OnePlus8.megaPixels)
-         * general_phone_thickness)/(4*OnePlus8.megaPixels)
-term2 = k*OnePlus8.apperature*general_phone_thickness/OnePlus8.megaPixels
+iPhoneX = Phone_Camera("https://en.wikipedia.org/wiki/IPhone_X")
+term1 = (-k*(iPhoneX.apperature) + 2*(iPhoneX.megaPixels)
+         * general_phone_thickness)/(4*iPhoneX.megaPixels)
+term2 = k*iPhoneX.apperature*general_phone_thickness/iPhoneX.megaPixels
+distance = -term1 + sqrt(term2 + term1*term1)
+print(distance)
+
+iPhoneXR = Phone_Camera("https://en.wikipedia.org/wiki/IPhone_XR")
+term1 = (-k*(iPhoneXR.apperature) + 2*(iPhoneXR.megaPixels)
+         * general_phone_thickness)/(4*iPhoneXR.megaPixels)
+term2 = k*iPhoneXR.apperature*general_phone_thickness/iPhoneXR.megaPixels
 distance = -term1 + sqrt(term2 + term1*term1)
 print(distance)
